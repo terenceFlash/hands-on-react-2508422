25c49b88afc28c845c5e7b40d899ea5a0e2aca15
terenceFlash <38274760+terenceFlash@users.noreply.github.com>
2022-11-14 19:28:48 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1>Meet the StarGazers</h1>
+      <h1>Meet the StarGazers...</h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
     </>
   )
